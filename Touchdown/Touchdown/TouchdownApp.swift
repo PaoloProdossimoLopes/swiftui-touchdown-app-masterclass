3017d6fc4f00a62e287b8db87cc1ba324e4e1e3b
Paolo Prodossimo Lopes <paolo.prodossimo.lopes@gmail.com>
2023-06-13 22:15:58 -0300

    chore: create project

---
+++ Touchdown/Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Paolo Prodossimo Lopes on 13/06/23.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
